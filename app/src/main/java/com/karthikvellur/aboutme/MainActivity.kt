cdec2575251619ac3b0bacfc491be756f9d0a59a
Karthik Vellur <karthik_vellur@intuit.com>
2021-05-25 21:21:34 +0530

    text view example

--- app/src/main/java/com/karthikvellur/aboutme/MainActivity.kt
+++ app/src/main/java/com/karthikvellur/aboutme/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 'R' means resource which is a 'layout' and the name of the file is 'activity_main'
+        // Hence R.layout.activity_main
     }
 }
