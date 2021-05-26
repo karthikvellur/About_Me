f6a1af8a4d2826414208b6a709b25e3d5fb04d2f
Karthik Vellur <karthik_vellur@intuit.com>
2021-05-26 07:53:01 +0530

    introduced data binding

--- app/src/main/java/com/karthikvellur/aboutme/MainActivity.kt
+++ app/src/main/java/com/karthikvellur/aboutme/MainActivity.kt
@@ -8,17 +8,26 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.karthikvellur.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // 'R' means resource which is a 'layout' and the name of the file is 'activity_main'
-        // Hence R.layout.activity_main
+        // 'R' means resource which is a 'layout' and the name of the file is 'activity_main'. Hence R.layout.activity_main
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         // Set the button onClick listener
-        findViewById<Button>(R.id.done_button).setOnClickListener {
-            addNickname(it) // 'it' is the view on which it is being set. In this case it is the 'done' button
+//        findViewById<Button>(R.id.done_button).setOnClickListener {
+//            addNickname(it) // 'it' is the view on which it is being set. In this case it is the 'done' button
+//        }
+
+        binding.doneButton.setOnClickListener{
+            addNickname(it)
         }
     }
 
@@ -27,13 +36,14 @@ class MainActivity : AppCompatActivity() {
      * In this case it is the 'Done' button
      */
     private fun addNickname(view: View){
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
-
-        nickNameTextView.text = editText.text
-        editText.visibility = View.GONE // EditText will be hidden
-        view.visibility = View.GONE // Button will be hidden
-        nickNameTextView.visibility = View.VISIBLE
+        // binding.apply is used to group the statements, instead of writing binding for every statement
+        binding.apply {
+            nicknameText.text = binding.nicknameEdit.text
+            invalidateAll() // Refreshes the UI with new data
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         // Hide the keyboard (After clicking on Done button)
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
